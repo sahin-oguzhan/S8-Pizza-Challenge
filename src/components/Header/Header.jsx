ac1f99a9a99d3a0302ed144b42b27bed3203e22f
sahin-oguzhan <oguzsahiinn@gmail.com>
2025-07-11 21:42:05 +0300

    Img path update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({ children }) {
   return (
     <header>
       <div className="logo">
-        <img src="/images/iteration-1-images/logo.svg" alt="" />
+        <img src="./images/iteration-1-images/logo.svg" alt="" />
       </div>
       <div className="nav">{children}</div>
     </header>
